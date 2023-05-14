c7b8bbe972e5324f546c8dfaf2a57994b78766c1
bodeodus <111711263+bodeodus@users.noreply.github.com>
2023-05-14 12:01:40 +0000

    adding oublic subnets and az config

--- bodeodusterrafom.tf
+++ bodeodusterrafom.tf
@@ -5,4 +5,7 @@ provider "aws" {
 module "bodeodusvpc" {
   source = "terraform-aws-modules/vpc/aws"
   cidr = "10.7.0.0/16"
+
+  azs = ["eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.7.100.0/24", "10.7.200.0/24"]
 }
